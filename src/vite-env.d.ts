76e1f017db550a8e92a55231957db0f7b8a09766
kanbaru-github <usupfv@gmail.com>
2025-02-02 12:23:58 +0900

    Add types env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_BOOKS_USER_ID: number;
+  readonly VITE_GOOGLE_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

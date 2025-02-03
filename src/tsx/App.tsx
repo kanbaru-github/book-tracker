e3c9a740ef0a9cb3d2b7afd2a0f3be2010007aa0
kanbaru-github <usupfv@gmail.com>
2025-02-04 08:48:52 +0900

    Add WAI-ARIA & relative pass specification

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -1,13 +1,13 @@
 import BookSearch from "./components/BookSearch";
 import BookList from "./components/BookList";
-import reactLogo from "/src/assets/react.svg";
-import "/src/scss/App.scss";
+import reactLogo from "./assets/react.svg";
+import "./scss/App.scss";
 
 function App() {
   return (
     <div className="max-w-6xl mx-auto p-4">
       <h1>
-        <img src={reactLogo} alt="React logo" />
+        <img src={reactLogo} alt="React logo" aria-hidden="true" />
         BOOK TRACKER
       </h1>
 

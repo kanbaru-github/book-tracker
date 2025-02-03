0b4fbd3489d9c22b4c82a64c0fd9494677a30bcd
kanbaru-github <usupfv@gmail.com>
2025-02-04 08:50:41 +0900

    Use semantic HTML

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -5,7 +5,7 @@ import "./scss/App.scss";
 
 function App() {
   return (
-    <div className="max-w-6xl mx-auto p-4">
+    <main className="max-w-6xl mx-auto p-4">
       <h1>
         <img src={reactLogo} alt="React logo" aria-hidden="true" />
         BOOK TRACKER
@@ -14,7 +14,7 @@ function App() {
       <BookSearch />
 
       <BookList />
-    </div>
+    </main>
   );
 }
 

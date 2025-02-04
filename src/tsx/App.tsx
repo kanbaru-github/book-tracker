f3f7ed20994ec37608ccab19fb223cc27384efc0
kanbaru-github <usupfv@gmail.com>
2025-02-04 23:49:36 +0900

    Fix relative pass

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -1,7 +1,7 @@
 import BookSearch from "./components/BookSearch";
 import BookList from "./components/BookList";
-import reactLogo from "./assets/react.svg";
-import "./scss/App.scss";
+import reactLogo from "../assets/react.svg";
+import "../scss/App.scss";
 
 function App() {
   return (

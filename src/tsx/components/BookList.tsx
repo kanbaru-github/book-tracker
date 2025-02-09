e2eb599fea6a55eb2a5c4cce8a114f45f86db745
kanbaru-github <usupfv@gmail.com>
2025-02-10 00:20:53 +0900

    5 Add empty case

--- src/tsx/components/BookList.tsx
+++ src/tsx/components/BookList.tsx
@@ -9,50 +9,52 @@ type BookListProps = {
 const BookList = ({ books }: BookListProps) => {
   return (
     <ul className="book-list">
-      { books ? books.map(book => (
-        <li key={book.id}>
-          {book.volumeInfo.imageLinks?.thumbnail && (
-            <img
-              src={book.volumeInfo.imageLinks.thumbnail}
-              alt={book.volumeInfo.title}
-              loading="lazy"
-            />
-          )}
+      {books ? (
+        books.map(book => (
+          <li key={book.id}>
+            {book.volumeInfo.imageLinks?.thumbnail && (
+              <img
+                src={book.volumeInfo.imageLinks.thumbnail}
+                alt={book.volumeInfo.title}
+                loading="lazy"
+              />
+            )}
 
-          {book.saleInfo.saleability === "FOR_SALE" && (
-            <a
-              href={book.saleInfo.buyLink}
-              className="book-list__sale"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              SALE!!
-              <ShoppingBasket size={16} />
-            </a>
-          )}
+            {book.saleInfo.saleability === "FOR_SALE" && (
+              <a
+                href={book.saleInfo.buyLink}
+                className="book-list__sale"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                SALE!!
+                <ShoppingBasket size={16} />
+              </a>
+            )}
 
-          <h3>
-            <a
-              href={book.volumeInfo.infoLink}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {book.volumeInfo.title}
-              <ExternalLink size={16} />
-            </a>
-          </h3>
+            <h3>
+              <a
+                href={book.volumeInfo.infoLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {book.volumeInfo.title}
+                <ExternalLink size={16} />
+              </a>
+            </h3>
 
-          <p className="author">
-            {book.volumeInfo.authors?.join(", ") || "著者不明"}
-          </p>
-          <p className="category">
-            {book.volumeInfo.categories?.join(", ") || "カテゴリ不明"}
-          </p>
-          <p className="description">
-            {book.volumeInfo.description || "説明不明"}
-          </p>
-        </li>
-      )) : (
+            <p className="author">
+              {book.volumeInfo.authors?.join(", ") || "著者不明"}
+            </p>
+            <p className="category">
+              {book.volumeInfo.categories?.join(", ") || "カテゴリ不明"}
+            </p>
+            <p className="description">
+              {book.volumeInfo.description || "説明不明"}
+            </p>
+          </li>
+        ))
+      ) : (
         <li className="book-list__empty">書籍が見つかりませんでした。</li>
       )}
     </ul>

cbd515a48be20404af66508e68d34d35e706b2bf
kanbaru-github <usupfv@gmail.com>
2025-02-09 09:28:46 +0900

    Add wai-aria

--- src/tsx/components/MainVisual.tsx
+++ src/tsx/components/MainVisual.tsx
@@ -4,9 +4,17 @@ const MainVisual: React.FC = () => {
   return (
     <section className="main-visual">
       <h1>BOOK TRACKER</h1>
-      <p className="main-visual__description">
+      <p
+        className="main-visual__description"
+        aria-label="Application description"
+      >
         書棚拡張アプリ by
-        <a href="https://books.google.co.jp/" target="_blank">
+        <a
+          href="https://books.google.co.jp/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Visit Google Books Japan"
+        >
           Google ブックス
         </a>
       </p>

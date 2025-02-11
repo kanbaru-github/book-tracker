5e3bc2b872886e0e87df0ff5ddc63cc1d0bd4bdc
zucky2021 <usupfv@gmail.com>
2025-02-12 00:05:49 +0900

    10 Add to top button

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -3,19 +3,39 @@ import Bookshelf from "./components/Bookshelf";
 import MainVisual from "./components/MainVisual";
 import "../scss/App.scss";
 import Header from "./components/Header";
+import { useEffect, useState } from "react";
+import { ArrowUp } from "lucide-react";
 
 function App() {
+  const [isTop, setIsTop] = useState(true);
+
+  useEffect(() => {
+    /**
+     * 最初の画面からスクロールしていたらfalseにする
+     */
+    const handleIsTop = () => {
+      setIsTop(scrollY === 0);
+    };
+
+    handleIsTop();
+    addEventListener("scroll", handleIsTop);
+  }, []);
+
   return (
     <>
       <Header />
 
-      <main className="max-w-6xl mx-auto p-4">
+      <main>
         <MainVisual />
 
         <BookSearch />
 
         <Bookshelf />
       </main>
+
+      <a href="#" className={`toTopBtn ${!isTop ? "toTopBtn--show" : ""}`}>
+        <ArrowUp size={24} />
+      </a>
     </>
   );
 }
